2761572b00c39b3f0aa5aa2caa17286143fdad3c
Jeremy Collins <jeremycollins@utah.gov>
2020-12-10 06:36:37 -0700

    starter code

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import com.company.model.Product;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+	// write your code here
+        System.out.println("Hello World");
+
+        Product product = new Product("keyboard");
+
+        System.out.println(product.getName());
+    }
+}
